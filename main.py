be590a2e64300c11d98194839e949db17cef11c3
sga-vuega <157767875+sga-vuega@users.noreply.github.com>
2025-03-24 23:13:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import google.generativeai as genai
 load_dotenv()
 
 # Set Streamlit page layout
-st.set_page_config(page_title="Excel Data Visualization with LLM",  layout="wide")
+st.set_page_config(page_title="Excel Data Visualization with LLM")
 
 MODEL_NAME = "gpt-4o"
 
